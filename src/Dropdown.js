2bdde3361a5178724a9199d0f525ba9e1b6f9fcf
Lyndon Panton <LyndonPanton@gmail.com>
2025-06-23 15:57:06 +0100

    Styled the dropdown menu

---
+++ src/Dropdown.js
@@ -0,0 +1,17 @@
+class Dropdown {
+    constructor(options, defaultOption) {
+        this.options = options;
+        this.defaultOption = defaultOption
+        this.main = document.getElementsByClassName("main")[0];
+    }
+
+    getDropdown() {
+        let dropdown = document.createElement("div");
+    }
+
+    getOptions() {
+        
+    }
+}
+
+export default Dropdown;
