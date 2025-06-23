df68ae2f08b3d17c87c5cdcb34ba8846fabdab5c
Lyndon Panton <LyndonPanton@gmail.com>
2025-06-23 17:58:19 +0100

    Feature: The icon on the dropdown changes when the dropdown is toggled

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import "./styles.css";
 
-function toggleDropdown(content) {
+function toggleDropdown(content, icon) {
     if (content.classList.contains("dropdown-content-hidden")) {
         content.classList.remove("dropdown-content-hidden");
+
+        icon.classList.remove("dropdown-display-icon-down");
+        icon.classList.add("dropdown-display-icon-up");
     } else {
         content.classList.add("dropdown-content-hidden");
+
+        icon.classList.remove("dropdown-display-icon-up");
+        icon.classList.add("dropdown-display-icon-down");
     }
 }
 
@@ -18,12 +24,13 @@ for (let i = 0; i < dropdowns.length; i++) {
     let dropdownContent = dropdowns[i].children[1];
     let dropdownContentItems = dropdownContent.children;
     let dropdownDisplay = dropdowns[i].children[0];
-    let dropdownButton = dropdownDisplay.children[0];
+    let dropdownDisplayButton = dropdownDisplay.children[0];
     
-    dropdownDisplay.addEventListener("click", toggleDropdown.bind(this, dropdownContent));
-    
+    let dropdownDisplayIcon = dropdownDisplay.children[1];
+    dropdownDisplay.addEventListener("click", toggleDropdown.bind(this, dropdownContent, dropdownDisplayIcon));
+
     for (let j = 0; j < dropdownContentItems.length; j++) {
-        dropdownContentItems[j].addEventListener("click", toggleDropdownOption.bind(this, dropdownButton));
+        dropdownContentItems[j].addEventListener("click", toggleDropdownOption.bind(this, dropdownDisplayButton));
     }
 
 }
