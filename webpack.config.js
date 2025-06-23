0737353a924c64636148b02cd2100b0f63e7d221
Lyndon Panton <LyndonPanton@gmail.com>
2025-06-23 15:17:44 +0100

    Configured html for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -7,5 +8,10 @@ module.exports = {
         filename: "main.js",
         path: path.join(__dirname, "dist"),
         clean: true
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
 };
