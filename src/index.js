f7a59d20280290a433769d2b54d110536ce530ff
Lyndon Panton <LyndonPanton@gmail.com>
2025-06-23 16:14:14 +0100

    Feature: Selected option of a dropdown can be toggled

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import "./styles.css";
 
+function toggleDropdown() {
+    console.log("Beep");
+}
+
+function toggleDropdownOption(dropdownButton, e) {
+    dropdownButton.textContent = e.target.textContent;
+}
+
+let dropdowns = document.getElementsByClassName("dropdown");
+
+for (let i = 0; i < dropdowns.length; i++) {
+    let dropdownButton = dropdowns[i].children[0];
+
+    dropdownButton.addEventListener("click", toggleDropdown);
+
+    let dropdownContentItems =
+            dropdowns[i].children[1]
+            .children;
+
+    for (let j = 0; j < dropdownContentItems.length; j++) {
+        dropdownContentItems[j].addEventListener("click", toggleDropdownOption.bind(this, dropdownButton));
+    }
+}
+
 console.log("Webpack configured successfully");
