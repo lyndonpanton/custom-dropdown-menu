e9d629bf1ccf7b3129af99b629744541e88fb800
Lyndon Panton <LyndonPanton@gmail.com>
2025-06-23 17:53:49 +0100

    Feature: Dropdown item visibility can be toggled

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import "./styles.css";
 
-function toggleDropdown() {
-    console.log("Beep");
+function toggleDropdown(content) {
+    if (content.classList.contains("dropdown-content-hidden")) {
+        content.classList.remove("dropdown-content-hidden");
+    } else {
+        content.classList.add("dropdown-content-hidden");
+    }
 }
 
 function toggleDropdownOption(dropdownButton, e) {
@@ -11,16 +15,17 @@ function toggleDropdownOption(dropdownButton, e) {
 let dropdowns = document.getElementsByClassName("dropdown");
 
 for (let i = 0; i < dropdowns.length; i++) {
-    let dropdownButton = dropdowns[i].children[0].children[0];
-    dropdownButton.addEventListener("click", toggleDropdown);
-
-    let dropdownContentItems =
-            dropdowns[i].children[1]
-            .children;
-
+    let dropdownContent = dropdowns[i].children[1];
+    let dropdownContentItems = dropdownContent.children;
+    let dropdownDisplay = dropdowns[i].children[0];
+    let dropdownButton = dropdownDisplay.children[0];
+    
+    dropdownDisplay.addEventListener("click", toggleDropdown.bind(this, dropdownContent));
+    
     for (let j = 0; j < dropdownContentItems.length; j++) {
         dropdownContentItems[j].addEventListener("click", toggleDropdownOption.bind(this, dropdownButton));
     }
+
 }
 
 console.log("Webpack configured successfully");
